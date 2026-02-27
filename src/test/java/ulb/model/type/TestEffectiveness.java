6d009c5dd860dc920476bb9b35db1d3ff04beaf7
Clément <clement.sinon@gmail.com>
2026-02-27 09:30:48 +0100

    edit: fixed effectiveness tests
    Co-authored-by: Abdalrahman El Hussein <abdalrahman.elhussein@ulb.be>

--- src/test/java/ulb/model/type/TestEffectiveness.java
+++ src/test/java/ulb/model/type/TestEffectiveness.java
@@ -18,17 +18,17 @@ public class TestEffectiveness {
 
 	@Test
 	public void verifyPyroToFlora() {
-		testTypeEffectiveness(Type.PYRO, Type.PYRO, Effectiveness.Value.NORMAL);
+		testTypeEffectiveness(Type.PYRO, Type.FLORA, Effectiveness.Value.NORMAL);
 	}
 
 	@Test
 	public void verifyPyroToAqua() {
-		testTypeEffectiveness(Type.PYRO, Type.PYRO, Effectiveness.Value.LOW);
+		testTypeEffectiveness(Type.PYRO, Type.AQUA, Effectiveness.Value.LOW);
 	}
 
 	@Test
 	public void verifyPyroToLitho() {
-		testTypeEffectiveness(Type.PYRO, Type.PYRO, Effectiveness.Value.HIGH);
+		testTypeEffectiveness(Type.PYRO, Type.LITHO, Effectiveness.Value.HIGH);
 	}
 
 	// Flora
@@ -40,17 +40,17 @@ public class TestEffectiveness {
 
 	@Test
 	public void verifyFloraToFlora() {
-		testTypeEffectiveness(Type.FLORA, Type.PYRO, Effectiveness.Value.NORMAL);
+		testTypeEffectiveness(Type.FLORA, Type.FLORA, Effectiveness.Value.NORMAL);
 	}
 
 	@Test
 	public void verifyFloraToAqua() {
-		testTypeEffectiveness(Type.FLORA, Type.PYRO, Effectiveness.Value.HIGH);
+		testTypeEffectiveness(Type.FLORA, Type.AQUA, Effectiveness.Value.HIGH);
 	}
 
 	@Test
 	public void verifyFloraToLitho() {
-		testTypeEffectiveness(Type.FLORA, Type.PYRO, Effectiveness.Value.LOW);
+		testTypeEffectiveness(Type.FLORA, Type.LITHO, Effectiveness.Value.LOW);
 	}
 
 	// Aqua
@@ -62,17 +62,17 @@ public class TestEffectiveness {
 
 	@Test
 	public void verifyAquaToFlora() {
-		testTypeEffectiveness(Type.AQUA, Type.PYRO, Effectiveness.Value.LOW);
+		testTypeEffectiveness(Type.AQUA, Type.FLORA, Effectiveness.Value.LOW);
 	}
 
 	@Test
 	public void verifyAquaToAqua() {
-		testTypeEffectiveness(Type.AQUA, Type.PYRO, Effectiveness.Value.NORMAL);
+		testTypeEffectiveness(Type.AQUA, Type.AQUA, Effectiveness.Value.NORMAL);
 	}
 
 	@Test
 	public void verifyAquaToLitho() {
-		testTypeEffectiveness(Type.AQUA, Type.PYRO, Effectiveness.Value.NORMAL);
+		testTypeEffectiveness(Type.AQUA, Type.LITHO, Effectiveness.Value.NORMAL);
 	}
 
 	// Litho
@@ -84,16 +84,16 @@ public class TestEffectiveness {
 
 	@Test
 	public void verifyLithoToFlora() {
-		testTypeEffectiveness(Type.LITHO, Type.PYRO, Effectiveness.Value.HIGH);
+		testTypeEffectiveness(Type.LITHO, Type.FLORA, Effectiveness.Value.HIGH);
 	}
 
 	@Test
 	public void verifyLithoToAqua() {
-		testTypeEffectiveness(Type.LITHO, Type.PYRO, Effectiveness.Value.NORMAL);
+		testTypeEffectiveness(Type.LITHO, Type.AQUA, Effectiveness.Value.NORMAL);
 	}
 
 	@Test
 	public void verifyLithoToLitho() {
-		testTypeEffectiveness(Type.LITHO, Type.PYRO, Effectiveness.Value.LOW);
+		testTypeEffectiveness(Type.LITHO, Type.LITHO, Effectiveness.Value.LOW);
 	}
 }

7a1db5d75426b10e1ee3db3f0e71dd3a85134a5d
Clément <clement.sinon@gmail.com>
2026-02-28 13:04:58 +0100

    add: battle snapshot tests
    Co-authored-by: Abdalrahman El Hussein <abdalrahman.elhussein@ulb.be>

--- src/main/java/ulb/model/battle/BattleSnapshot.java
+++ src/main/java/ulb/model/battle/BattleSnapshot.java
@@ -2,16 +2,31 @@ package ulb.model.battle;
 
 import ulb.model.team.Team;
 
+/**
+ * View of the battle from the point of view of a player.
+ */
 public class BattleSnapshot {
 	private Battle battle;
 	private boolean isTeamA;
 
-	BattleSnapshot(Battle battle){
+	/**
+	 * Creates a battle snapshot as the point of view of either team A or team A.
+	 *
+	 * @param battle The battle
+	 * @param isTeamA True if the battle is viewed from team A's poiint of view, false otherwise
+	 */
+	BattleSnapshot(Battle battle, boolean isTeamA){
 		this.battle = battle;
+		this.isTeamA = isTeamA;
 	}
 
+	/**
+	 * Gives the team that the snapshot views itself as.
+	 *
+	 * @return The snapshot's own team
+	 */
 	public Team getTeamSelf(){
-		if (isTeamA) {
+		if (this.isTeamA) {
 			return battle.getTeamA();
 		}
 		else {
@@ -19,8 +34,13 @@ public class BattleSnapshot {
 		}
 	}
 
+	/**
+	 * Gives the team that the snapshot views its opponent as.
+	 *
+	 * @return The snapshot's opponent team
+	 */
 	public Team getTeamOpponent(){
-		if (! isTeamA) {
+		if (this.isTeamA) {
 			return battle.getTeamB();
 		}
 		else {

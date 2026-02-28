5a65c879798ad8adb62af04861b1f558a7e52014
LUCKIE <elhusseinabdalrahman@gmail.com>
2026-02-28 12:47:50 +0100

    add: strcture of BattleSnapshotTest

---
+++ src/test/java/ulb/model/battle/BattleSnapshotTest.java
@@ -0,0 +1,14 @@
+package ulb.model.battle;
+
+import org.junit.Test;
+
+public class BattleSnapshotTest {
+	private BattleSnapshot getBattleSnapshot(){
+	}
+
+	@Test
+	public void testTeamASelf(){
+
+	}
+
+}

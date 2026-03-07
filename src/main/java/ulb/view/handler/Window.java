964e3e747be0d84c9df410a3c23eac67e4f43086
LUCKIE <elhusseinabdalrahman@gmail.com>
2026-03-07 23:34:40 +0100

    add: switchWindow methode
    Co-authored-by: Daniel Blasiak <daniel.blasiak@ulb.be>

--- src/main/java/ulb/view/handler/Window.java
+++ src/main/java/ulb/view/handler/Window.java
@@ -2,16 +2,19 @@ package ulb.view.handler;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public abstract class Window{
-	//TODO
-	private Parent root;
-	public void switchWindow() throws IOException{
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/ulb/view/CreateTeamWindow.fxml"));
-		root = loader.load();
+	public final String windowsPath= "/ulb/view/";
+	public void switchWindow(ActionEvent event, String windowFxmlPath) throws IOException{
+		Parent root = FXMLLoader.load(getClass().getResource(windowFxmlPath));
+
+  		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		stage.getScene().setRoot(root);
 	}
 	public String getMessage(WindowName uiName, String messageType){return "";};
 	public String sendMessage(WindowName uiName, String messageType){return "";};
--- src/main/java/ulb/view/windows/ModeWindow.java
+++ src/main/java/ulb/view/windows/ModeWindow.java
@@ -1,17 +1,20 @@
 package ulb.view.windows;
 
+
 import java.io.IOException;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import ulb.view.handler.Window;
 
 final public class ModeWindow extends Window{
+	public final String nextWindow= "CreateTeamWindow.fxml";
 
 	@FXML
-	private void goSoloMode() {
+	private void goSoloMode(ActionEvent event) {
 		try{
-			switchWindow();
+			switchWindow(event, windowsPath+nextWindow);
 		}
 		catch (IOException e){
 			throw new RuntimeException("Cannot load FXML", e);

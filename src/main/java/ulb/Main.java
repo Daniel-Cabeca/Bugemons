49b1cb5733f2bec00396a961977cb840358a2d1c
Clément <clement.sinon@ulb.be>
2026-03-09 11:22:04 +0100

    edit: fixed missing data loading

--- src/main/java/ulb/Main.java
+++ src/main/java/ulb/Main.java
@@ -2,8 +2,16 @@ package ulb;
 import javafx.application.Application;
 import ulb.view.windows.MainWindow;
 
+import ulb.model.Loader;
+
 public class Main {
 	public static void main(String[] args){
-		Application.launch(MainWindow.class,args);
+		try {
+			Loader.load();
+			Application.launch(MainWindow.class,args);
+		} catch (Exception e) {
+			System.err.println("Uncaught error.");
+			System.err.println(e.getMessage());
+		}
 	}
 }

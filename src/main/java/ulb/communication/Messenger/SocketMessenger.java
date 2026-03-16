7ad79b3c4ef932a02e44871892aa375f2a89b892
Alexis Smeets <alexis.smeets@ulb.be>
2026-03-16 15:02:17 +0100

    add: Client-Server Communication

---
+++ src/main/java/ulb/communication/Messenger/SocketMessenger.java
@@ -0,0 +1,59 @@
+package ulb.communication.Messenger;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import ulb.communication.Message;
+
+public class SocketMessenger implements Messenger {
+    private Socket socket;
+    private ObjectInputStream reader;
+    private ObjectOutputStream writer;
+
+    public SocketMessenger(Socket socket){
+        this.socket = socket;
+        try{
+
+            this.writer = new ObjectOutputStream(this.socket.getOutputStream());
+            this.reader = new ObjectInputStream(this.socket.getInputStream());
+            
+        } catch (Exception e){
+            this.close();
+            System.err.println(e);
+        }
+    }
+
+    public void SendMessage(Message message){
+        try{
+            this.writer.writeObject(message);
+            this.writer.reset();
+        } catch (Exception e){
+            this.close();
+            System.err.println(e);
+        }
+    }
+
+    public Message receiveMessage(){
+        Message message = null;
+        try{
+            message = (Message) reader.readObject();
+        } catch (Exception e){
+            this.close();
+            System.err.println(e);
+        }
+        return message;
+    }
+
+    public void close(){
+        if (socket != null){
+            try{
+                this.socket.close();
+                this.reader.close();
+                this.writer.close();
+            } catch (Exception e){
+                System.err.println(e);
+            }
+        }
+    }
+}

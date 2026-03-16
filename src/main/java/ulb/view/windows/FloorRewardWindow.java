a955a233fcdb65255b005a5c79e01e9f63678efc
LUCKIE <elhusseinabdalrahman@gmail.com>
2026-03-16 12:31:20 +0100

    add: FloorRewardWindow
    Co-authored-by: Rugile Banaityte <rugile.banaityte@ulb.be>

---
+++ src/main/java/ulb/view/windows/FloorRewardWindow.java
@@ -0,0 +1,21 @@
+package ulb.view.windows;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class FloorRewardWindow {
+	@FXML
+	private Button rewardA;
+	@FXML
+	private Button rewardB;
+	@FXML
+	private Button rewardC;
+
+	@FXML
+	private void rewardA(){}
+	@FXML
+	private void rewardB(){}
+	@FXML
+	private void rewardC(){}
+
+}

c4babddc27a190992641eb768776da1df6dd401f
Clément <clement.sinon@gmail.com>
2026-04-05 01:10:58 +0200

    edit: use in-memory db for tests where possible

--- src/test/java/ulb/repository/database/DatabaseConnectionTest.java
+++ src/test/java/ulb/repository/database/DatabaseConnectionTest.java
@@ -6,21 +6,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class DatabaseConnectionTest {
 	@Test
 	public void isOpenAfterOpen() {
-		DatabaseConnection dbConnection = new DatabaseConnection("jdbc:sqlite:mock.db");
+		DatabaseConnection dbConnection = new DatabaseConnection("jdbc:sqlite::memory:");
 		dbConnection.open();
 		assertTrue(dbConnection.isOpen());
+
+		dbConnection.close();
 	}
 
 	@Test
 	public void openThrowsIfAlreadyOpen() {
-		DatabaseConnection dbConnection = new DatabaseConnection("jdbc:sqlite:mock.db");
+		DatabaseConnection dbConnection = new DatabaseConnection("jdbc:sqlite::memory:");
 		dbConnection.open();
 		assertThrows(IllegalStateException.class, () -> { dbConnection.open(); });
 	}
 
 	@Test
 	public void isNotOpenAfterClose() {
-		DatabaseConnection dbConnection = new DatabaseConnection("jdbc:sqlite:mock.db");
+		DatabaseConnection dbConnection = new DatabaseConnection("jdbc:sqlite::memory:");
 		dbConnection.open();
 		dbConnection.close();
 		assertFalse(dbConnection.isOpen());
@@ -28,7 +30,7 @@ class DatabaseConnectionTest {
 
 	@Test
 	public void closeDoesNotThrowIfNotOpen() {
-		DatabaseConnection dbConnection = new DatabaseConnection("jdbc:sqlite:mock.db");
+		DatabaseConnection dbConnection = new DatabaseConnection("jdbc:sqlite::memory:");
 		assertDoesNotThrow(() -> { dbConnection.close(); });
 	}
 }

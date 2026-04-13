80fb5ab6a48cd692ee2be16f0a520542c7868112
ZIA GULZAR <zia.gulzar@ulb.be>
2026-04-13 23:25:06 +0200

    delete: javadoc

--- src/main/java/ulb/model/action/Run.java
+++ src/main/java/ulb/model/action/Run.java
@@ -7,7 +7,7 @@ import ulb.model.battle.Battle.ParticipantLabel;
  * Action representing a battle forfeit.
  */
 public class Run implements Action {
-    /** Creates a run action. */
+
     public Run() {}
 
     /** {@inheritDoc} */

e7a2438e518e6dd5101572b192473be13dc976db
ZIA GULZAR <zia.gulzar@ulb.be>
2026-04-13 23:35:29 +0200

    add: javadoc modeWindow

--- src/main/java/ulb/view/windows/ModeWindow.java
+++ src/main/java/ulb/view/windows/ModeWindow.java
@@ -3,7 +3,10 @@ package ulb.view.windows;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 import ulb.utils.Scaling;
-
+/**
+ * Class for the Mode selection window.
+ * Handles UI interactions and scaling for the main menu options.
+ */
 public class ModeWindow {
 
 	@FXML
@@ -15,25 +18,35 @@ public class ModeWindow {
 	public void initialize() {
 		Scaling.applyScaling(content);
 	}
-
+	/**
+	 * Notifies the action to open the social panel.
+	 */
 	@FXML
 	private void openSocialPanel() {
 		viewListener.onOpenSocial();
 	}
-
+	/**
+	 * Notifies the action to start the solo game mode.
+	 */
 	@FXML
 	private void goSoloMode() {
 		viewListener.onSolo();
 	}
-
+	/**
+	 * Notifies the action to start the multiplayer game mode.
+	 */
 	@FXML
 	private void goMultiplayer() {	viewListener.onMultiplayer(); }
-
+	/**
+	 * Notifies the action to quit the application.
+	 */
 	@FXML
 	private void quit() {
 		viewListener.onQuit();
 	}
-
+	/**
+	 * Notifies the action to log the user out of the current session.
+	 */
 	@FXML
 	private void logOut() { viewListener.onLogOut(); }
 

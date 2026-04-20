5ae072cec0b901b180d02277627d78190730bc41
LUCKIE <elhusseinabdalrahman@gmail.com>
2026-04-20 21:49:59 +0200

    add: connect floorwindow.fxml to floorwindow.java

---
+++ src/main/java/ulb/view/windows/FloorWindow.java
@@ -0,0 +1,54 @@
+package ulb.view.windows;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class FloorWindow {
+    @FXML
+    Label floor;
+    @FXML
+    Button boss;
+    @FXML
+    Button battleA1;
+    @FXML
+    Button battleA2;
+    @FXML
+    Button battleB;
+    @FXML
+    Button bonusA;
+    @FXML
+    Button bonusB;
+    @FXML
+    Button player;
+
+    @FXML
+    private void onBoss(){
+
+    }
+    @FXML
+    private void onBattleA1(){
+
+    }
+    @FXML
+    private void onBattleA2(){
+
+    }
+    @FXML
+    private void onBattleB(){
+
+    }
+    @FXML
+    private void onBonusA(){
+
+    }
+    @FXML
+    private void onBonusB(){
+
+    }
+    @FXML
+    private void onPlayer(){
+
+    }
+
+}

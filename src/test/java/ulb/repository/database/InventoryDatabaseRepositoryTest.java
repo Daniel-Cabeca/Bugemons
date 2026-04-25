be9d9bf827e374741799ed9a01c5f2196a87d5e3
snad0007 <sam.nadif@ulb.be>
2026-04-25 19:05:56 +0200

    edit: add new tests for InventoryDatabaseRepository

--- src/test/java/ulb/repository/database/InventoryDatabaseRepositoryTest.java
+++ src/test/java/ulb/repository/database/InventoryDatabaseRepositoryTest.java
@@ -21,67 +21,93 @@ import java.util.NoSuchElementException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InventoryDatabaseRepositoryTest {
+    ItemDatabaseRepository itemRepository;
+    InventoryDatabaseRepository inventoryRepository;
+    String username;
+    Item item1;
+    Item item2;
 
     private void insertUser(String username, Database database) throws LoadException {
         AccountDatabaseRepository accountRepository = new AccountDatabaseRepository(database);
         accountRepository.register(username, "password");
     }
 
-    @Test
-    public void testInsertItemAndGetInventory() throws DuplicateElementException, LoadException, NoSuchElementException {
+    private void initiateDatabaseAndRepositories() throws LoadException, DuplicateElementException {
         Database database = new DatabaseInMemory();
         DatabaseInitializer databaseInitializer = new DatabaseInitializer(database);
         databaseInitializer.createTables();
 
-        ItemDatabaseRepository itemRepository = new ItemDatabaseRepository(database);
-        InventoryDatabaseRepository inventoryRepository = new InventoryDatabaseRepository(database, itemRepository);
+        this.itemRepository = new ItemDatabaseRepository(database);
+        this.inventoryRepository = new InventoryDatabaseRepository(database, itemRepository);
 
-        String username = "testUsername";
+        this.username = "testUsername";
         this.insertUser(username, database);
 
         Effect effect = new EffectHeal(EffectTarget.OPPOSITE_BUGEMON,5);
 
-    	Item item1 = new Item("1","potion","","", effect, "");
-    	Item item2 = new Item("2","potion2","","", effect, "");
-        itemRepository.insertItem(item1);
-        itemRepository.insertItem(item2);
+    	this.item1 = new Item("1","potion","","", effect, "");
+    	this.item2 = new Item("2","potion2","","", effect, "");
+        this.itemRepository.insertItem(item1);
+        this.itemRepository.insertItem(item2);
+    }
 
-        inventoryRepository.insertItem(item1, 1, username);
-        inventoryRepository.insertItem(item2, 1, username);
+    @Test
+    public void testInsertItemAndGetInventory() throws DuplicateElementException, LoadException, NoSuchElementException {
+        initiateDatabaseAndRepositories();
 
-        Inventory inventoryFromDatabase = inventoryRepository.getInventory(username);
+        this.inventoryRepository.insertItem(this.item1, 1, this.username);
+        this.inventoryRepository.insertItem(this.item2, 1, this.username);
+
+        Inventory inventoryFromDatabase = this.inventoryRepository.getInventory(this.username);
 
         assertEquals(2, inventoryFromDatabase.getItems().size());
     }
 
     @Test
     public void testInsertInventoryAndGetInventory() throws DuplicateElementException, LoadException, NoSuchElementException {
-        Database database = new DatabaseInMemory();
-        DatabaseInitializer databaseInitializer = new DatabaseInitializer(database);
-        databaseInitializer.createTables();
-
-        ItemDatabaseRepository itemRepository = new ItemDatabaseRepository(database);
-        InventoryDatabaseRepository inventoryRepository = new InventoryDatabaseRepository(database, itemRepository);
-
-        String username = "testUsername";
-        this.insertUser(username, database);
-
-        Effect effect = new EffectHeal(EffectTarget.OPPOSITE_BUGEMON,5);
-
-    	Item item1 = new Item("1","potion","","", effect, "");
-    	Item item2 = new Item("2","potion2","","", effect, "");
-        itemRepository.insertItem(item1);
-        itemRepository.insertItem(item2);
+        initiateDatabaseAndRepositories();
 
         Inventory inventoryToAdd = new Inventory();
-        inventoryToAdd.addItem(item1, 1);
-        inventoryToAdd.addItem(item2, 1);
+        inventoryToAdd.addItem(this.item1, 1);
+        inventoryToAdd.addItem(this.item2, 1);
 
-        inventoryRepository.insertInventory(inventoryToAdd, username);
+        this.inventoryRepository.insertInventory(inventoryToAdd, this.username);
 
-        Inventory inventoryFromDatabase = inventoryRepository.getInventory(username);
+        Inventory inventoryFromDatabase = this.inventoryRepository.getInventory(this.username);
 
         assertEquals(2, inventoryFromDatabase.getItems().size());
        
+    }
+
+    @Test 
+    public void testDeleteItem() throws DuplicateElementException, LoadException, NoSuchElementException {
+        initiateDatabaseAndRepositories();
+
+        this.inventoryRepository.insertItem(this.item1, 1, this.username);
+        this.inventoryRepository.insertItem(this.item2, 1, this.username);
+
+        this.inventoryRepository.deleteItem(this.item2, 1, this.username);
+
+        Inventory inventoryFromDatabase = this.inventoryRepository.getInventory(this.username);
+
+        assertEquals(1, inventoryFromDatabase.getItems().size());
+    }
+
+    @Test
+    public void updateInventory() throws DuplicateElementException, LoadException, NoSuchElementException {
+        initiateDatabaseAndRepositories();
+        
+        this.inventoryRepository.insertItem(this.item1, 1, this.username);
+
+        Inventory updatedInventory = new Inventory();
+        updatedInventory.addItem(this.item1, 2);
+        updatedInventory.addItem(item2, 1);
+
+        this.inventoryRepository.updateInventory(updatedInventory, this.username);
+
+        Inventory inventoryFromDatabase = this.inventoryRepository.getInventory(this.username);
+
+        assertEquals(2, inventoryFromDatabase.getItems().size());
+        assertEquals(2, inventoryFromDatabase.getQuantity(item1));
     }
 }

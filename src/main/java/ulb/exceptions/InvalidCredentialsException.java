fbcdf6349c689d1c65d19ab0b6d8cfc337c375e0
Daniel Blasiak <daniel.blasiak@ulb.be>
2026-05-22 19:13:49 +0200

    add: added missing comments to /exceptions

--- src/main/java/ulb/exceptions/InvalidCredentialsException.java
+++ src/main/java/ulb/exceptions/InvalidCredentialsException.java
@@ -1,5 +1,8 @@
 package ulb.exceptions;
 
+/**
+ * Exception thrown when invalid credentials are provided during authentication.
+ */
 public class InvalidCredentialsException extends Exception {
 	public InvalidCredentialsException() {
 		super("Wrong password");
